17da54dfc7e0dc20859c239f2f86d93946e8a82f
Chris <cdiana.media@gmail.com>
2015-02-21 13:54:21 -0500

    landing view stufffff

--- Heinzight/ViewControllers/Landing/AgeSelectionViewController.cs
+++ Heinzight/ViewControllers/Landing/AgeSelectionViewController.cs
@@ -1,31 +1,50 @@
-﻿
-using System;
+﻿using System;
 using System.Drawing;
 
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
+using Heinzight.Core;
+
 namespace Heinzight
 {
+	public delegate void OnAgeSelected(CurrentUser.AgeOptions age);
+
 	public partial class AgeSelectionViewController : UIViewController
 	{
+		public event OnAgeSelected OnAgeSelected;
+
 		public AgeSelectionViewController () : base ("AgeSelectionViewController", null)
 		{
-		}
-
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
+
+			SetCircleButtonDisplay (littleKidViewButton);
+			SetCircleButtonDisplay (adultViewButton);
+
+			var adultTapGesture = new UITapGestureRecognizer (OptionSelected(CurrentUser.AgeOptions.Adult));
+			adultView.AddGestureRecognizer (adultTapGesture);
+
+			var childTapGesture = new UITapGestureRecognizer (OptionSelected(CurrentUser.AgeOptions.Child));
+			littleKidView.AddGestureRecognizer (childTapGesture);
+
+		}
+
+		static void SetCircleButtonDisplay(UIView v)
+		{
+			v.Layer.CornerRadius = 17;
+			v.Layer.BorderColor = UIColor.LightGray.CGColor;
+			v.BackgroundColor = UIColor.White;
+			v.Layer.BorderWidth = 2.0f;
+		}
+
+		NSAction OptionSelected(CurrentUser.AgeOptions age) 
+		{
+			OnAgeSelected (age);
+			return null;
 		}
 	}
 }
--- Heinzight/ViewControllers/Landing/AgeSelectionViewController.designer.cs
+++ Heinzight/ViewControllers/Landing/AgeSelectionViewController.designer.cs
@@ -1,18 +1,66 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace Heinzight
 {
 	[Register ("AgeSelectionViewController")]
 	partial class AgeSelectionViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIView adultView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIView adultViewButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel introTextLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIView littleKidView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIView littleKidViewButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel questionLabel { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (adultView != null) {
+				adultView.Dispose ();
+				adultView = null;
+			}
+
+			if (adultViewButton != null) {
+				adultViewButton.Dispose ();
+				adultViewButton = null;
+			}
+
+			if (littleKidView != null) {
+				littleKidView.Dispose ();
+				littleKidView = null;
+			}
+
+			if (littleKidViewButton != null) {
+				littleKidViewButton.Dispose ();
+				littleKidViewButton = null;
+			}
+
+			if (introTextLabel != null) {
+				introTextLabel.Dispose ();
+				introTextLabel = null;
+			}
+
+			if (questionLabel != null) {
+				questionLabel.Dispose ();
+				questionLabel = null;
+			}
 		}
 	}
 }
