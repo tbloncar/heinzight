9a8b22c64e9609636849963e97c2b258180a62b0
Travis Loncar <loncar.travis@gmail.com>
2015-02-21 17:05:38 -0500

    Add method for getting displays associated w/ chosen interests.

---
+++ Heinzight.Core/Data Access/DisplayDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Device.Location;
+
+using Heinzight.Core.ORM;
+
+namespace Heinzight.Core
+{
+	namespace DAO
+	{
+		public class DisplayDAO
+		{
+			public static List<Display> GetDisplaysForInterests(List<Interest> interests)
+			{
+				List<int> interestIds = interests.Select (i => i.ID).ToList();
+				var displays = new List<Display> ();
+
+				using (var conn = Db.Instance.GetConnection ()) {
+					displays = (List<Display>)conn.Table<DisplayInterest> ()
+						.Where (di => interestIds.Contains (di.InterestId))
+						.Select(di => di.Display).Distinct().ToList();
+				}
+
+				return displays;
+			}
+		}
+	}
+}
+
