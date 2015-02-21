6d66ef9a9f789e0c992b65689fc3b10a44aacbc9
Cameron Ray <cameron@cameronray.net>
2015-02-21 12:02:42 -0500

    add CurrentUser

---
+++ Heinzight.Core/CurrentUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Autofac;
+
+namespace Heinzight.Core
+{
+	public class CurrentUser
+	{
+		private static CurrentUser instance;
+
+		private CurrentUser() {}
+
+		public static CurrentUser Instance
+		{
+			get 
+			{
+				if (instance == null)
+				{
+					instance = new CurrentUser();
+				}
+				return instance;
+			}
+		}
+	}
+}
