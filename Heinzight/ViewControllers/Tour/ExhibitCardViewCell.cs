ae548fb81ff93729670350304e99dd88e220381d
Chris <cdiana.media@gmail.com>
2015-02-22 00:26:33 -0500

    fml

---
+++ Heinzight/ViewControllers/Tour/ExhibitCardViewCell.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Heinzight
+{
+	public class ExhibitCardViewCell : UICollectionViewCell
+	{
+		public static readonly NSString Key = new NSString ("ExhibitCardViewCell");
+
+		[Export ("initWithFrame:")]
+		public ExhibitCardViewCell (RectangleF frame) : base (frame)
+		{
+			// TODO: add subviews to the ContentView, set various colors, etc.
+			BackgroundColor = UIColor.Cyan;
+		}
+	}
+}
+
