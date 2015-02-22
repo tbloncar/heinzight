c37a5ab71d06f1579c0a5ff4c14b61836a0317d8
Pete Mullins <petermullins@openarc.net>
2015-02-22 07:43:25 -0500

    merge

---
+++ Heinzight/ViewControllers/Landing/DetailsViewController.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.IO;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Heinzight
+{
+	public partial class DetailsViewController : UIViewController
+	{
+		public DetailsViewController () : base ("DetailsViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			var exhibitContent = File.ReadAllText ("");
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			exhibitView.LoadHtmlString(exhibitContent,null);
+		}
+	}
+}
+
---
+++ Heinzight/ViewControllers/Landing/DetailsViewController.designer.cs
@@ -0,0 +1,34 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace Heinzight
+{
+	[Register ("DetailsViewController")]
+	partial class DetailsViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UILabel exhibitTitleLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIWebView exhibitView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (exhibitTitleLabel != null) {
+				exhibitTitleLabel.Dispose ();
+				exhibitTitleLabel = null;
+			}
+
+			if (exhibitView != null) {
+				exhibitView.Dispose ();
+				exhibitView = null;
+			}
+		}
+	}
+}
